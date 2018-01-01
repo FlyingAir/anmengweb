e0d5407b7c919ee96e1ac0b3d04ec1648d9f9655
FlyingAir <827199132@qq.com>
2018-01-01 22:47:33 +0800

    增静态资源

--- js/index.js
+++ js/index.js
@@ -1,18 +1,40 @@
-$(function(){
-    var mySwiper = new Swiper ('.swiper-container', {
+$(function() {
+    var mySwiper = new Swiper('.swiper-container', {
         loop: true,
-        effect : 'fade',
+        effect: 'fade',
         fade: {
-          crossFade: true,
+            crossFade: true,
         },
+        autoplay: 3000,
         // 如果需要分页器
         pagination: '.swiper-pagination',
-        
+
         // 如果需要前进后退按钮
         // nextButton: '.swiper-button-next',
         // prevButton: '.swiper-button-prev',
-        
+
         // 如果需要滚动条
         // scrollbar: '.swiper-scrollbar',
-      })        
+    });
+    // 下拉框效果
+    //子导航展开收缩
+    $(".sewvtop").click(function() {
+        $(this).find("em").removeClass("lbaxztop2").addClass("lbaxztop").parents(".sewv").siblings().children(".sewvtop").find("em").removeClass("lbaxztop").addClass(".lbaxztop2");
+        $(this).next(".sewvbm").toggle().parents(".sewv").siblings().find(".sewvbm").hide();
+    });
+
+    /*鼠标离开下拉框关闭*/
+    $(".sewv").mouseleave(function() {
+        $(".sewvbm").hide();
+        $(this).children(".sewvtop").find("em").addClass("lbaxztop2");
+    });
+
+
+    //赋值
+    $(".sewvbm>li").click(function() {
+        var selva = $(this).text();
+        $(this).parents(".sewvbm").siblings(".sewvtop").find("span").text(selva);
+        $(this).parent("ul").hide();
+        $(this).parents(".sewv").children(".sewvtop").find("em").addClass("lbaxztop2");
+    });
 })
